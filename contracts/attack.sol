35137ebe5f98559ff4475659f746a38fe05b3860
hellobloc <113746997+Hellobloc@users.noreply.github.com>
2023-04-15 13:38:08 +0800

    Update and rename library.sol to attack.sol

---
+++ contracts/attack.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.0;
+import "./$.{37}|2{40}|cantbematchedcharacters__";
+contract A {
+    address constant public a = address(0x2222222222222222222222222222222222222222);
+    uint public b;
+    function cc() public{
+        b = L_.get4();
+    }
+}
--- contracts/library.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.6.0;
-import "./LibraryForTest.sol";
- contract test {
-    function get2 () pure public returns(uint) {
-        return LibraryForTest.get4();    
-    }
-}
